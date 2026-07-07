819b80b9b982cfb995e6a179722e373252dbb2c1
Raul Batres <rabp90@gmail.com>
2026-07-07 14:45:49 -0600

    Update api-config.ts

--- inventory-app/src/app/core/api-config.ts
+++ inventory-app/src/app/core/api-config.ts
@@ -1,2 +1,2 @@
 //export const API_BASE_URL = 'http://localhost:8080/api/v1';
-export const API_BASE_URL = ' https://stockflow-x9eb.onrender.com';
+export const API_BASE_URL = 'https://stockflow-x9eb.onrender.com/api/v1';
